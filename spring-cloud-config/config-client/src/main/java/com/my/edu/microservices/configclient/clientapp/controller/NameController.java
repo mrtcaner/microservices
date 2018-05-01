4f58b8a2a7448e543bf1fc98e19eec3b6961adf5
mrtcaner <muratcaner@ymail.com>
2018-05-01 20:29:51 +0300

    added refresh scope annotation

--- spring-cloud-config/config-client/src/main/java/com/my/edu/microservices/configclient/clientapp/controller/NameController.java
+++ spring-cloud-config/config-client/src/main/java/com/my/edu/microservices/configclient/clientapp/controller/NameController.java
@@ -1,6 +1,7 @@
 package com.my.edu.microservices.configclient.clientapp.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by lenovo510 on 30.04.2018.
  */
 @RestController
+@RefreshScope
 public class NameController {
 
     @Value("${prefixes}")
